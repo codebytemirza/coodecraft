7eadfc2ffb35bd718123bd28103b089414cb39a8
codebytemirza <abdullah.codebyte@gmail.com>
2024-12-31 22:56:04 +0500

    contact

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,13 +2,13 @@ export const SITE_CONFIG = {
     name: 'Logixcell',
     description: 'Transforming Careers Through Code',
     contact: {
-      phone: '+923088403978',
+      phone: '+923277222004',
       email: 'logixcellacademy@gmail.com',
       address: 'Silicon Valley, CA'
     },
     social: {
-      twitter: 'https://twitter.com/techmaster',
-      github: 'https://github.com/techmaster',
-      linkedin: 'https://linkedin.com/techmaster'
+      twitter: '#',
+      github: '#',
+      linkedin: 'https://linkedin.com/logixcell',
     }
   };
